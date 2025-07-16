9c2e9f63a9c5f192bc037207e76b853cfc62bd5e
raymondlin7510 <raymond.lin7510@berkeley.edu>
2025-07-16 12:48:28 -0700

    Set up the API on Visual Studio

---
+++ api/backend/backend/Controllers/TrackerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    
+    [ApiController]
+    public class TrackerController : ControllerBase
+    {
+        private IConfiguration configuration;
+        public TrackerController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+    }
+}
