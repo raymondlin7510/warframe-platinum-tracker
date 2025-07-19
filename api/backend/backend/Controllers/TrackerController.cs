c58f03bd96e6c412320b18999c589a08343540d2
Larry Zhou <zhoularry15@gmail.com>
2025-07-18 17:28:11 -0700

    finished setting up database, get, add, and delete methods

--- api/backend/backend/Controllers/TrackerController.cs
+++ api/backend/backend/Controllers/TrackerController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace backend.Controllers
 {
@@ -7,11 +10,75 @@ namespace backend.Controllers
     [ApiController]
     public class TrackerController : ControllerBase
     {
-        private IConfiguration configuration;
+        private IConfiguration _configuration;
         public TrackerController(IConfiguration configuration)
         {
-            this.configuration = configuration;
+            this._configuration = configuration;
         }
 
+        [HttpGet("get_all")]
+        public JsonResult get_all()
+        {
+            string query = "select * from plat_list";
+            DataTable table = new DataTable();
+            string SqlDatasource = _configuration.GetConnectionString("wfdb");
+            SqlDataReader myHeader;
+            using (SqlConnection myCon = new SqlConnection(SqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myHeader = myCommand.ExecuteReader();
+                    table.Load(myHeader);
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost("add_item")]
+        public JsonResult add_item([FromForm] string item, int price)
+        {
+            string query = "insert into plat_list(checked, item , price) values (0, @item, @price)";
+            DataTable table = new DataTable();
+            string SqlDatasource = _configuration.GetConnectionString("wfdb");
+            SqlDataReader myHeader;
+            using (SqlConnection myCon = new SqlConnection(SqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@item", item);
+                    myCommand.Parameters.AddWithValue("@price",price);
+                    myHeader = myCommand.ExecuteReader();
+                    table.Load(myHeader);
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPost("delete_task")]
+        public JsonResult delete_task([FromForm] string id)
+        {
+            string query = "delete from plat_list where id=@id";
+            DataTable table = new DataTable();
+            string SqlDatasource = _configuration.GetConnectionString("wfdb");
+            SqlDataReader myHeader;
+            using (SqlConnection myCon = new SqlConnection(SqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@id", id);
+                    myHeader = myCommand.ExecuteReader();
+                    table.Load(myHeader);
+                }
+            }
+            return new JsonResult("Deleted Successfully");
+        }
+
+
+
     }
+
+
 }
--- api/backend/backend/obj/Release/net8.0/backend.AssemblyInfo.cs
+++ api/backend/backend/obj/Release/net8.0/backend.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2e9f63a9c5f192bc037207e76b853cfc62bd5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
