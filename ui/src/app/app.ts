de2018576847baadb9e2d122cfc5f5a16d7b0ccc
raymondlin7510 <raymond.lin7510@berkeley.edu>
2025-07-21 00:24:46 -0700

    Added a layout for the frontend for html and the app component without using any angular modules

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,HttpClientModule,FormsModule,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ui');
+
+  items:any=[];
+  newitem="";
+
+  APIURL="http://localhost:8000/";
+
+  constructor(private http:HttpClient){}
+
+  
 }
